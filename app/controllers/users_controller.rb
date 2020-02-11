95e0b34a0a8c11050670d023a430b9fbe2f94347
leduccuong1612 <leduccuong1612@gmail.com>
2020-02-11 09:16:09 +0700

    Update,Delete,Show User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.page(params[:page]).per Settings.paginate.length
+    @users = User.page(params[:page]).per Settings.controller.paginate_length
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)",locale: /en|vi/ do
+  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
     root "static_pages#home"
 
     get "/help", to: "static_pages#help"
